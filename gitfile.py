83c9cd12e7d38617d058b5794cc96aea444ff53e
Issibombae13 <99311673+Issibombae13@users.noreply.github.com>
2022-03-27 21:42:27 -0500

    clean up code

--- gitfile.py
+++ gitfile.py
@@ -1,9 +1,6 @@
 # "This is just for testing"
 
 print(" Happy Holidays ")
-<<<<<<< HEAD
 Print(" second edit ")
 print(" Third edit ")
 print(" Fourth edit ")
-=======
->>>>>>> parent of 0095371 (Second update to gitfile.py)
